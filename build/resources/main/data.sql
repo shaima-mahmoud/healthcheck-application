10e3b70cf7e72ad481662c2e768731694b70d8eb
shima <shima.mahmoud.90@gmail.com>
2020-09-25 16:57:49 +0200

    delete build folder

--- build/resources/main/data.sql
+++
@@ -1,3 +0,0 @@
-insert into country (code, name, continent, region, indep_year) values ('001', 'name1', 'cont', 'region1', 2000);
-
-insert into country_language (country_code, language, is_official, percentage) values ('001', 'Arabic', 'true', 50.0);
